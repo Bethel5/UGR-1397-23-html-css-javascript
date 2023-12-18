b470bd0f806c6ab48904db03d4df02fb9addb543
Bethel5 <betheldereje5@gmail.com>
2023-12-18 22:31:51 +0300

    add javascript

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.querySelector("container__header__navigation__menu__icon");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
